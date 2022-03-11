51c46d24fe175b4986484087e9fce3217f28fe67
srgrealey <srgrealey@gmail.com>
2022-03-11 15:25:49 -0500

    Initial Commit

---
+++ PersonalScheduler/PersonalSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalSchedulerApp.swift
+//  PersonalScheduler
+//
+//  Created by Seamus Grealey on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PersonalSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
